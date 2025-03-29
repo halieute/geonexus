5769a94b9ab3c7ab31799a4a5f6636a519d9d49e
Souleymane Maman Nouri Souley <ssouley@uta.cv>
2025-03-29 01:46:44 -0100

    Add split map

--- geonexus/foliumap.py
+++ geonexus/foliumap.py
@@ -1,7 +1,7 @@
 """This module provides a custom Map class that extends folium.Map."""
 
 import folium
-
+import folium.plugins
 
 class Map(folium.Map):
     def __init__(self, center=(0, 0), zoom=2, **kwargs):
@@ -94,3 +94,14 @@ class Map(folium.Map):
         The layer control allows users to toggle the visibility of layers.
         """
         folium.LayerControl().add_to(self)
+
+
+    def add_split_map(self, right='openstreetmap', left='cartodbpositron', **kwargs):
+        layer_right = folium.TileLayer(right, **kwargs)
+        layer_left = folium.TileLayer(left, **kwargs)
+
+        sbs = folium.plugins.SideBySideLayers(layer_left=layer_left, layer_right=layer_right)
+
+        layer_left.add_to(self)
+        layer_right.add_to(self)
+        sbs.add_to(self)
